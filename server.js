232ef717eaf0b77e8d04e6ad0374e115097f87d8
Wys <noahgaming951@gmail.com>
2023-02-08 08:52:30 +0100

    Creating language support

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 // Setup our requirements
-require("better-console")
-const Prompt = require('prompt-choices');
+const console = require('better-console');
+const { ADDRGETNETWORKPARAMS } = require('dns');
+const { AutoComplete, Select, Scale } = require('enquirer');
 const fs = require("fs")
 const pkg = require(`${__dirname}/package.json`)
+let lang = "English"
 
 // Print SplashScreen
-fs.readFile(`${__dirname}/splash.txt`, 'utf8', async function(err, data){
-    console.clear()
-    console.info(`\n${data}`)
-    console.info(`      v${pkg.version} by ${pkg.author}`)
+var data = fs.readFileSync('splash.txt', 'utf8');
+console.clear()
+console.info(`\n${data}`)
+console.info(`      v${pkg.version} by ${pkg.author}\n\n`)
+
+// Ask for lang
+const lang_choice = new Select({
+    name: 'lang',
+    message: 'Choose a language',
+    choices: ['English', 'French']
 });
 
-// Ask for choice
+// Run the prompt
+lang_choice.run()
+    .then(answer => {
+        lang = answer
+    })
+    .catch(console.error);
+
+// Ask for function
+const menu_choice = new Select({
+    name: 'menu',
+    message: 'Choisissez une fonction',
+    choices: ["Générateur d'images", "Générateur de texte", "Générateur d'entrées"]
+})
