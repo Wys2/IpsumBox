50068ddffc0a42fcc99f2df37c5582d5d525d409
Wys <noahgaming951@gmail.com>
2023-02-24 15:58:23 +0100

    Commiting text module

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 // Setup our requirements
 const console = require('better-console');
-const { ADDRGETNETWORKPARAMS } = require('dns');
-const { AutoComplete, Select, Scale } = require('enquirer');
+const { AutoComplete, Select, Scale, NumberPrompt, Confirm } = require('enquirer');
 const fs = require("fs")
+const fastLoremIpsum = require('fast-lorem-ipsum');
 const pkg = require(`${__dirname}/package.json`)
 let lang = require("./lang/en.json")
 
 // Print SplashScreen
 var data = fs.readFileSync('splash.txt', 'utf8');
 console.clear()
-console.info(`\n${data}`)
-console.info(`      v${pkg.version} by ${pkg.author}\n\n`)
+console.log(`\n${data}`)
+console.log(`      v${pkg.version} by ${pkg.author}\n\n`)
 
 // Ask for lang
 const lang_choice = new Select({
@@ -19,13 +19,44 @@ const lang_choice = new Select({
     choices: ['en', 'fr']
 });
 
+// Save to file function
+async function saveToFile(input){
+    console.info(l.results.sucess_stf)
+}
+
+// Define function
+async function eval_fn(answer){
+    if(answer == l.general.titles.txt_gen){
+
+        new NumberPrompt({
+            name: 'txt_gen_n',
+            message: l.txt_gen.amt
+        }).run().then(num => {
+            new Confirm({
+                name: 'savetofile',
+                message: l.general.titles.stf
+              }).run().then(result => {
+                rs = fastLoremIpsum(`${num}w`)
+                if(result){
+                    saveToFile(rs)
+                } else console.log(rs)
+              })
+        })
+
+    } else if(answer == l.general.titles.img_gen){
+
+    } else if(answer == l.general.titles.entry_gen){
+
+    } else console.error("Unhandled functionnality")
+}
+
 // Run the prompt
 lang_choice.run()
     .then(answer => {
         l = require(`./lang/${answer}.json`)
         new Select({
             name: 'menu',
-            message: l.titles.choices.fn_choose,
-            choices: [l.titles.choices.txt_gen, l.titles.choices.img_gen, l.titles.choices.entry_gen]
-        }).run()
+            message: l.general.titles.fn_choose,
+            choices: [l.general.titles.txt_gen, l.general.titles.img_gen, l.general.titles.entry_gen]
+        }).run().then(answer => eval_fn(answer))
     })
